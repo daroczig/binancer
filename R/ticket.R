066947dd7b000554bd0d7407e91350d9aea83584
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-01-12 12:24:31 +0100

    compute to/from + prices in USD(T)

--- R/ticket.R
+++ R/ticket.R
@@ -7,6 +7,21 @@ binance_ticker_all_prices <- function() {
     prices <- binance_query(endpoint = 'api/v1/ticker/allPrices')
     prices <- rbindlist(prices)
     prices[, price := as.numeric(price)]
-    prices
+
+    ## not sure what's this
+    prices <- prices[symbol != '123456']
+
+    ## split from/to
+    prices[, from := sub('(ETH|BTC|USD|BNB)$', '', symbol)]
+    prices[, to := sub('.*(ETH|BTC|USD|BNB)$', '\\1', symbol)]
+
+    ## add computed price in USD
+    prices[grepl('ETH$', symbol), to_usd := prices[symbol == 'ETHUSDT', price]]
+    prices[grepl('BTC$', symbol), to_usd := prices[symbol == 'BTCUSDT', price]]
+    prices[grepl('USDT$', symbol), to_usd := price]
+    prices[grepl('BNB$', symbol), to_usd := prices[symbol == 'BNBUSDT', price]]
+    prices[, from_usd := price * to_usd]
+
+    prices[, .(symbol, price, from, from_usd, to, to_usd)]
 
 }
