627bf68d06d66385ed9be98454157ee14c565e1d
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-01-12 11:22:19 +0100

    add limit and tradeId params when fetching most recent trades

--- R/account.R
+++ R/account.R
@@ -9,6 +9,8 @@ binance_account <- function() {
 
 #' Get all trades on the Binance account
 #' @param symbol string
+#' @param limit number of trades to fetch
+#' @param from_id optional trade id to fetch from
 #' @return data.table
 #' @export
 #' @importFrom data.table as.data.table
@@ -17,17 +19,18 @@ binance_account <- function() {
 #' binance_mytrades(c('ARKBTC', 'ARKETH'))
 #' }
 #' @importFrom snakecase to_snake_case
-binance_mytrades <- function(symbol) {
+binance_mytrades <- function(symbol, limit = 500, from_id) {
 
     if (length(symbol) > 1) {
         return(rbindlist(lapply(symbol, binance_mytrades), fill = TRUE))
     }
 
-    trades <- binance_query(
-        endpoint = 'api/v3/myTrades',
-        params = list(symbol = symbol),
-        sign = TRUE)
+    params <- list(symbol = symbol)
+    if (!missing(from_id)) {
+        params$fromId = from_id
+    }
 
+    trades <- binance_query(endpoint = 'api/v3/myTrades', params = params, sign = TRUE)
     trades <- rbindlist(trades)
     if (nrow(trades) == 0) {
         return(data.table())
