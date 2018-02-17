d95cc95eac91810d9a84318609beec4f048da7d0
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-02-17 14:13:06 +0100

    add fn to get Huobi account id

--- R/huobi.R
+++ R/huobi.R
@@ -2,16 +2,24 @@
 #' @param endpoint string
 #' @param method HTTP request method
 #' @param params list
+#' @param sign if signature required
 #' @param retry allow retrying the query on failure
 #' @return R object
 #' @keywords internal
 #' @importFrom jsonlite fromJSON
 huobi_query <- function(endpoint, method = 'GET',
-                          params = list(),
+                          params = list(), sign = FALSE,
                           retry = method == 'GET') {
 
     method <- match.arg(method)
-    config <- config()
+
+    if (isTRUE(sign)) {
+        ## TODO
+        params <- binance_sign(params)
+        config <- add_headers('X-MBX-APIKEY' = binance_key())
+    } else {
+        config <- config()
+    }
 
     fromJSON(rawToChar(query(
         base = 'https://api.huobi.pro',
@@ -39,4 +47,12 @@ huobi_klines <- function(symbol,
         method = 'GET',
         params = match.call()[[-1]])
 
+}
+
+
+#' Get Huobi accountid
+#' @return string
+#' @export
+huobi_accountid <- function() {
+    huobi_query(endpoint = 'v1/account/accounts', sign = TRUE)
 }
