b9ef2fc9df655b74f7878b1670ed435f1ed93a20
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2021-12-18 00:37:13 +0800

    Move spot specific tests to test-spot.R

--- tests/testthat/test-binance.R
+++ tests/testthat/test-binance.R
@@ -1,5 +1,3 @@
-as_time <- function(number) as.POSIXct(number, origin = "1970-01-01")
-
 test_that("Sign query", {
     key <- "foo"
     secret <- "bar"
@@ -15,60 +13,4 @@ test_that("Sign query", {
         signed_list$signature,
         "b6d60f421e0481f16af34b46d1db6577786cf8688538a3f8ca76bb1bc5b90d3a"
     )
-})
-
-test_that("Ping on Spot", {
-    vcr::use_cassette("spot_ping", {
-        response <- binance_ping()
-    })
-
-    expect_equal(response, "OK")
-})
-
-test_that("Time on Spot", {
-    vcr::use_cassette("spot_time", {
-        response <- binance_time()
-    })
-
-    expect_equal(response, as_time(1639747098))
-})
-
-test_that("Exchange info on Spot", {
-    vcr::use_cassette("spot_exchange_info", {
-        response <- binance_exchange_info()
-    })
-
-    expect_equal(
-        response$serverTime,
-        as_time(1639754298)
-    )
-})
-
-test_that("Klines on Spot", {
-    vcr::use_cassette("spot_klines_ethusdt_1d_limit_1", {
-        response <- binance_klines("ETHUSDT", interval = "1d", limit = 1)
-    })
-
-    expect_equal(response$symbol, "ETHUSDT")
-    expect_equal(
-        response$open_time,
-        as_time(1639699200)
-    )
-    expect_equal(
-        response$close_time,
-        as_time(1639785600)
-    )
-    expect_equal(response$open, 3959.1, tolerance = 0.01)
-    expect_equal(response$high, 3959.1, tolerance = 0.01)
-    expect_equal(response$low, 3796, tolerance = 0.01)
-    expect_equal(response$close, 3821, tolerance = 0.01)
-    expect_equal(response$volume, 210423, tolerance = 0.01)
-    expect_equal(response$quote_asset_volume, 819993474)
-    expect_equal(response$trades, 470771)
-    expect_equal(
-        response$taker_buy_base_asset_volume,
-        105358.2,
-        tolerance = 0.01
-    )
-    expect_equal(response$taker_buy_quote_asset_volume, 411011816)
 })
