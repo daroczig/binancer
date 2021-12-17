7985d76019b9c1d860af1de4f7f6ba1b91924e1f
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2021-12-17 21:35:55 +0800

    Test spot klines

--- tests/testthat/test-binance.R
+++ tests/testthat/test-binance.R
@@ -13,3 +13,32 @@ test_that("Binance Time on Spot", {
 
     expect_equal(response, as.POSIXct(1639747098, origin = "1970-01-01"))
 })
+
+test_that("Binance Klines on Spot", {
+    vcr::use_cassette("binance_spot_klines_ethusdt_1d_limit_1", {
+        response <- binance_klines("ETHUSDT", interval = "1d", limit = 1)
+    })
+
+    expect_equal(response$symbol, "ETHUSDT")
+    expect_equal(
+        response$open_time,
+        as.POSIXct(1639699200, origin = "1970-01-01")
+    )
+    expect_equal(
+        response$close_time,
+        as.POSIXct(1639785600, origin = "1970-01-01")
+    )
+    expect_equal(response$open, 3959.1, tolerance = 0.01)
+    expect_equal(response$high, 3959.1, tolerance = 0.01)
+    expect_equal(response$low, 3796, tolerance = 0.01)
+    expect_equal(response$close, 3821, tolerance = 0.01)
+    expect_equal(response$volume, 210423, tolerance = 0.01)
+    expect_equal(response$quote_asset_volume, 819993474)
+    expect_equal(response$trades, 470771)
+    expect_equal(
+        response$taker_buy_base_asset_volume,
+        105358.2,
+        tolerance = 0.01
+    )
+    expect_equal(response$taker_buy_quote_asset_volume, 411011816)
+})
