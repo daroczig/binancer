c9e601068f522d9f8f85f5bbc4be5884ba50aee0
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2021-12-17 21:47:48 +0800

    Move api_key and secret out

--- tests/testthat/setup-binancer.R
+++ tests/testthat/setup-binancer.R
@@ -1,9 +1,12 @@
 library("vcr") # *Required* as vcr is set up on loading
 
+api_key <- Sys.getenv("BINANCE_API_KEY")
+secret <- Sys.getenv("BINANCE_SECRET")
+
 invisible(vcr::vcr_configure(
     filter_sensitive_data = list(
-        "<<<api_key>>>" = Sys.getenv("BINANCE_API_KEY"),
-        "<<<secret>>>" = Sys.getenv("BINANCE_SECRET")
+        "<<<api_key>>>" = api_key,
+        "<<<secret>>>" = secret
     ),
     dir = vcr::vcr_test_path("fixtures")
 ))
