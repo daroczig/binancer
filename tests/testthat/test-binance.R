81f4f102b44134352dbf17350666417d9894c865
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2021-12-17 21:50:35 +0800

    Rename fixtures

--- tests/testthat/test-binance.R
+++ tests/testthat/test-binance.R
@@ -1,21 +1,21 @@
-test_that("Binance Ping on Spot", {
-    vcr::use_cassette("binance_spot_ping", {
+test_that("Ping on Spot", {
+    vcr::use_cassette("spot_ping", {
         response <- binance_ping()
     })
 
     expect_equal(response, "OK")
 })
 
-test_that("Binance Time on Spot", {
-    vcr::use_cassette("binance_spot_time", {
+test_that("Time on Spot", {
+    vcr::use_cassette("spot_time", {
         response <- binance_time()
     })
 
     expect_equal(response, as.POSIXct(1639747098, origin = "1970-01-01"))
 })
 
-test_that("Binance Klines on Spot", {
-    vcr::use_cassette("binance_spot_klines_ethusdt_1d_limit_1", {
+test_that("Klines on Spot", {
+    vcr::use_cassette("spot_klines_ethusdt_1d_limit_1", {
         response <- binance_klines("ETHUSDT", interval = "1d", limit = 1)
     })
 
