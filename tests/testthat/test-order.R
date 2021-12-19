701de73d3e7d2d4aa26174c87972e00c792bff48
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2021-12-20 02:14:10 +0800

    Test is_algo_order()

--- tests/testthat/test-order.R
+++ tests/testthat/test-order.R
@@ -5,3 +5,14 @@ test_that("usdm_limit_order", {
     expect_equal(order$quantity, 120)
     expect_equal(order$side, "BUY")
 })
+
+test_that("is_algo_order", {
+    expect_false(is_algo_order(structure(list(), class = "LIMIT")))
+    expect_true(is_algo_order(structure(list(), class = "STOP")))
+    expect_true(is_algo_order(structure(list(), class = "STOP_MARKET")))
+    expect_true(is_algo_order(structure(list(), class = "TAKE_PROFIT")))
+    expect_true(is_algo_order(structure(list(), class = "TAKE_PROFIT_MARKET")))
+    expect_true(
+        is_algo_order(structure(list(), class = "TRAILING_STOP_MARKET"))
+    )
+})
